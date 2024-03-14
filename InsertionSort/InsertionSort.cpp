bfaaadd15e715c708a2fa22b53e36adc120a5767
SuperXRanzo <okanskyavenger@gmail.com>
2024-03-14 17:33:42 +0700

    Menambah InsertShort

--- InsertionSort/InsertionSort.cpp
+++ InsertionSort/InsertionSort.cpp
@@ -1,9 +1,23 @@
 #include <iostream>
 using namespace std;
 
-void insertionshort() {
+void InsertionShort() {
 	int i, n, temp, j;
 	int arr[9];
+
+	for (i = 1; <= n - 1; i++) { //step 1
+
+		temp = arr[i]; //step 2
+		
+		j = i - 1; //step 3
+
+		while (j >= 0 && arr[j] > temp) //step 3b
+		{
+			arr[j + 1] = arr[j]; //step 4a
+			j--; //step 4b
+		}
+		arr[j + 1] = temp; //step 5
+	}
 }
 
 int main()
